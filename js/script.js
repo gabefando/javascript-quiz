1d8a7a05a6ec27fa98bf44fbc1fce6f251556392
gabefando <gabe.fonseca.04@gmail.com>
2022-06-28 23:02:29 -0400

    defined variables, created working idea for quiz

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var startBtn = document.getElementById("startBtn");
+var aBtn = document.getElementById("aBtn");
+var bBtn = document.getElementById("bBtn");
+var cBtn = document.getElementById("cBtn");
+var dBtn = document.getElementById("dBtn");
+var question = getElementById("question");
+var questionNum = 0;
+
+function quizStart() {
+    console.log("quiz started")
+}
+
+startBtn.addEventListener("click", quizStart);
